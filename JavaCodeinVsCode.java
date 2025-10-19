8394530969887f6d1176a62c47a78539ea54612a
satyadeep <satyadeepupriya@gmail.com>
2025-10-19 16:04:59 +0530

    First commit after many commits

---
+++ JavaCodeinVsCode.java
@@ -0,0 +1,8 @@
+public class JavaCodeinVsCode{
+
+    public static void main(String args[]){
+        System.out.println("Hello Git, Welcome to my World");
+        System.out.println("Hello World");
+        System.out.println("Hello World");
+    }
+}
